467680c44a82e453cd788240ed9e671ba9a62930
Clay <thinkclay@gmail.com>
2025-09-19 09:22:09 -0400

    feat: add tamagui drawer navigation

--- apps/expo/app/(drawer)/_layout.tsx
+++ apps/expo/app/(drawer)/_layout.tsx
@@ -1,6 +1,31 @@
-import { ProfileScreen } from '@my/app/features/profile/screen'
+import { DrawerMenu } from '@my/app/features/drawer-menu'
+import { getTokens, useMedia } from '@my/ui'
 import { Drawer } from 'expo-router/drawer'
 
 export default function Layout() {
-  return <Drawer drawerContent={ProfileScreen} />
+  const media = useMedia()
+  const tokens = getTokens()
+  const drawerWidth = media.gtSm ? tokens.size['$20'].val : tokens.size['$10'].val
+
+  return (
+    <Drawer
+      screenOptions={{
+        headerShown: false,
+        drawerType: media.gtSm ? 'permanent' : 'front',
+        swipeEnabled: !media.gtSm,
+        overlayColor: 'rgba(10,10,10,0.15)',
+        drawerStyle: {
+          width: drawerWidth,
+          backgroundColor: 'transparent',
+        },
+        sceneContainerStyle: {
+          backgroundColor: 'transparent',
+        },
+        drawerContentStyle: {
+          padding: 0,
+        },
+      }}
+      drawerContent={(props) => <DrawerMenu {...props} />}
+    />
+  )
 }
