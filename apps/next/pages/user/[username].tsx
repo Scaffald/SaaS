8f5d0dd87dcb26e87887cc3e5121dd78cadb1a8d
Clay Unicorn <thinkclay@gmail.com>
2025-09-26 07:55:08 -0400

    Add public profile data hook and review summary

--- apps/next/pages/user/[username].tsx
+++ apps/next/pages/user/[username].tsx
@@ -1,20 +1,41 @@
 import { HomeLayout } from '@app/core/features/home/layout.web'
 import { PublicProfileScreen } from '@app/core/features/profile/public-profile-screen'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
 import type { NextPageWithLayout } from '../_app'
 
-const Page: NextPageWithLayout = () => {
+type PageProps = {
+  username: string
+}
+
+const Page: NextPageWithLayout<PageProps> = ({ username }) => {
   return (
     <>
       <Head>
         <title>Public Profile</title>
       </Head>
-      <PublicProfileScreen />
+      <PublicProfileScreen username={username} />
     </>
   )
 }
 
 Page.getLayout = (page) => <HomeLayout headerTitle="Public profile">{page}</HomeLayout>
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async ({ params }) => {
+  const username = params?.username
+
+  if (typeof username !== 'string' || username.length === 0) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      username,
+    },
+  }
+}
 
 export default Page
