e0787a0bab9637a4849fcfa48c20b1cc6a7d37cf
Clay Unicorn <thinkclay@gmail.com>
2025-09-26 04:27:05 -0400

    feat: add public profile view

---
+++ apps/next/pages/profile/[username].tsx
@@ -0,0 +1,23 @@
+import { HomeLayout } from '@app/core/features/home/layout.web'
+import { PublicProfileScreen } from '@app/core/features/profile/public-profile-screen'
+import Head from 'next/head'
+
+import type { NextPageWithLayout } from '../_app'
+
+const Page: NextPageWithLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Public Profile</title>
+      </Head>
+      <PublicProfileScreen />
+    </>
+  )
+}
+
+Page.getLayout = (page) => (
+  <HomeLayout headerTitle="Public profile">{page}</HomeLayout>
+)
+
+export default Page
+
