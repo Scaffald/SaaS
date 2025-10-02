e3a685e801737e752ab2241eb291c74953adda56
Clay <thinkclay@gmail.com>
2025-10-01 21:29:25 -0400

    Map: cards jump map to pin

--- packages/ui/src/components/maps/index.ts
+++ packages/ui/src/components/maps/index.ts
@@ -1,8 +1,19 @@
-export { MapContainer } from './MapContainer'
-export { MapPin } from './MapPin'
-export { MapTooltip } from './MapTooltip'
-export { mockMapPins, defaultMapCenter, defaultMapZoom, defaultRadius } from './mockData'
-export type { MapPin as MapPinType, MapContainerProps, MapTooltipData, MapRegion } from './types'
+export { MapContainer } from "./MapContainer";
+export { MapPin } from "./MapPin";
+export { MapTooltip } from "./MapTooltip";
+export {
+  defaultMapCenter,
+  defaultMapZoom,
+  defaultRadius,
+  mockMapPins,
+} from "./mockData";
+export type {
+  MapContainerProps,
+  MapPin as MapPinType,
+  MapRegion,
+  MapTooltipData,
+} from "./types";
+export type { MapContainerRef } from "./MapContainer";
 
 // Web-specific MapBox components are NOT exported here to avoid breaking React Native
 // They should be imported directly: import { MapComponent, MapMarker } from '@app/ui/src/components/maps/Map.web'
