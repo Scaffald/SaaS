9521438d05673df2150aaeb0a317a06cf51552c1
Clay <thinkclay@gmail.com>
2025-10-11 17:19:09 -0400

    feat: add multi-taxonomy skills system with industry-based filtering
    - Create new skillsV2 router with support for CSI and O*NET taxonomies
    - CSI skills only available for construction industry
    - O*NET skills available for all industries
    - Add onet.search_occupations() function
    - Wire up skillsMultiTaxonomyRouter to profile router
    - Polymorphic user_skills table supports both taxonomies

--- packages/supabase/functions/trpc/routers/profile/index.ts
+++ packages/supabase/functions/trpc/routers/profile/index.ts
@@ -7,6 +7,7 @@ import { profileEmploymentRouter } from "./employment.router.ts";
 import { profileExperienceRouter } from "./experience.router.ts";
 import { profileGeneralRouter } from "./general.router.ts";
 import { profileSkillsRouter } from "./skills.router.ts";
+import { skillsMultiTaxonomyRouter } from "./skills-multi-taxonomy.router.ts";
 
 /**
  * Profile router - merges all profile-related sub-routers
@@ -15,6 +16,10 @@ export const profileRouter = t.mergeRouters(
   profileGeneralRouter,
   profileEmploymentRouter,
   profileSkillsRouter,
+  t.router({
+    // Multi-taxonomy skills endpoints
+    skillsV2: skillsMultiTaxonomyRouter,
+  }),
   profileAvatarRouter,
   profileCertificationsRouter,
   profileEducationRouter,
