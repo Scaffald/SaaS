8ac3a952049a3214b147097f1efd3405925b80f5
Clay <thinkclay@gmail.com>
2025-11-06 01:19:39 -0500

    Oops

---
+++ apps/expo/app/dashboard/assessments/pulse/index.tsx
@@ -0,0 +1,9 @@
+import { LuscherTestWizard } from '@app/core/features/luscher-test'
+
+/**
+ * Luscher Test Assessment Page
+ * Displays the unified Luscher Color Test wizard
+ */
+export default function LuscherTestPage() {
+  return <LuscherTestWizard />
+}
