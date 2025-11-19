cdc7fda267625f309e64e4cff595ef75a06fd38d
Clay <thinkclay@gmail.com>
2025-11-19 08:56:09 -0500

    feat(search-select): add core types and hooks

---
+++ packages/ui/src/components/search-select/index.ts
@@ -0,0 +1,4 @@
+export * from './types'
+export { useSearch } from './hooks/useSearch'
+export { useSearchSelect } from './hooks/useSearchSelect'
+export { useKeyboardNav } from './hooks/useKeyboardNav'
